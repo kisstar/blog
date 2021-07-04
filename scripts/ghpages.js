00bd9e0901cdbbec748e0ac3f92e19fcc95c24d4
kisstar <dwh.chn@foxmail.com>
2021-07-04 15:13:36 +0800

    chore: 发布时自动添加 CNAME 文件

--- scripts/ghpages.js
+++ scripts/ghpages.js
@@ -1,21 +1,31 @@
+const path = require('path');
+const fs = require('fs');
 const ghpages = require('gh-pages');
 
+const config = {
+  dir: '.vuepress/dist',
+  cname: 'CNAME',
+};
 const logger = {
-    log: console.log,
-    error: console.error,
-}
+  log: console.log,
+  error: console.error,
+};
 const options = {
-    repo: 'https://github.com/kisstar/kisstar.github.io.git',
-    branch: 'master',
-    dir: '.vuepress/dist'
-}
+  repo: 'https://github.com/kisstar/kisstar.github.io.git',
+  branch: 'master',
+};
+const resolve = (...args) => path.resolve(__dirname, '../', ...args);
 
-ghpages.publish(options.dir, options, function(err) {
-    if (err) {
-        logger.error('============ 发布失败 ============\n');
-        logger.error(err);
-        return
-    }
+fs.writeFileSync(
+  resolve(config.dir, config.cname),
+  fs.readFileSync(config.cname)
+);
+ghpages.publish(config.dir, options, function(err) {
+  if (err) {
+    logger.error('============ 发布失败 ============\n');
+    logger.error(err);
+    return;
+  }
 
-    logger.log('============ 发布成功 ============');
+  logger.log('============ 发布成功 ============');
 });
