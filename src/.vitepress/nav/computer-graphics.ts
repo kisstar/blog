ff19e91cc347482e5a7a59ae0ea7055de5dfd5ad
Kisstar <dwh.chn@foxmail.com>
2024-03-22 23:38:55 +0800

    docs(cg): 图形软件

--- src/.vitepress/nav/computer-graphics.ts
+++ src/.vitepress/nav/computer-graphics.ts
@@ -2,7 +2,8 @@ import { type DefaultTheme } from 'vitepress';
 
 const computerGraphics: DefaultTheme.SidebarItem[] = [
   { text: '计算机图形学', link: 'index.html' },
-  { text: '硬件', link: 'hardware.html' }
+  { text: '图形硬件', link: 'hardware.html' },
+  { text: '图形软件', link: 'software.html' }
 ];
 
 export default computerGraphics;
