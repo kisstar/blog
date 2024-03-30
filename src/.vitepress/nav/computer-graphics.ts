8f5631bd1c7d5e377f16c7633a7ae2488eaa2b8e
Kisstar <dwh.chn@foxmail.com>
2024-03-30 11:27:45 +0800

    docs(cg): 图像是如何渲染到屏幕的

--- src/.vitepress/nav/computer-graphics.ts
+++ src/.vitepress/nav/computer-graphics.ts
@@ -3,7 +3,8 @@ import { type DefaultTheme } from 'vitepress';
 const computerGraphics: DefaultTheme.SidebarItem[] = [
   { text: '计算机图形学', link: 'index.html' },
   { text: '图形硬件', link: 'hardware.html' },
-  { text: '图形软件', link: 'software.html' }
+  { text: '图形软件', link: 'software.html' },
+  { text: '图像是如何渲染到屏幕的', link: 'render2screen.html' }
 ];
 
 export default computerGraphics;
