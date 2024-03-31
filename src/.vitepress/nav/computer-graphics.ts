4bcbc78e647ebbe8fc7a08f81d92d2491c661584
Kisstar <dwh.chn@foxmail.com>
2024-03-31 14:40:09 +0800

    docs(cg): 图形渲染管线

--- src/.vitepress/nav/computer-graphics.ts
+++ src/.vitepress/nav/computer-graphics.ts
@@ -4,7 +4,8 @@ const computerGraphics: DefaultTheme.SidebarItem[] = [
   { text: '计算机图形学', link: 'index.html' },
   { text: '图形硬件', link: 'hardware.html' },
   { text: '图形软件', link: 'software.html' },
-  { text: '图像是如何渲染到屏幕的', link: 'render2screen.html' }
+  { text: '图像是如何渲染到屏幕的', link: 'render2screen.html' },
+  { text: '图形渲染管线', link: 'pipeline.html' }
 ];
 
 export default computerGraphics;
