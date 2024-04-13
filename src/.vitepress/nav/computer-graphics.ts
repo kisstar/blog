8cdb4390df3db4b5bc9fac4480ac719d8523533a
Kisstar <dwh.chn@foxmail.com>
2024-04-13 14:56:33 +0800

    docs(cg): 在 OpenGL 中的图形渲染管线

--- src/.vitepress/nav/computer-graphics.ts
+++ src/.vitepress/nav/computer-graphics.ts
@@ -5,7 +5,8 @@ const computerGraphics: DefaultTheme.SidebarItem[] = [
   { text: '图形硬件', link: 'hardware.html' },
   { text: '图形软件', link: 'software.html' },
   { text: '图像是如何渲染到屏幕的', link: 'render2screen.html' },
-  { text: '图形渲染管线', link: 'pipeline.html' }
+  { text: '图形渲染管线', link: 'pipeline.html' },
+  { text: 'OpenGL 的图形渲染管线', link: 'opengl-pipeline.html' }
 ];
 
 export default computerGraphics;
