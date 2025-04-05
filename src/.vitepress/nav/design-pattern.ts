11938755b61b8f19e29baf946644276ac939ad33
Kisstar <dwh.chn@foxmail.com>
2025-04-05 14:47:47 +0800

    docs(design-pattern): 软件设计原则

--- src/.vitepress/nav/design-pattern.ts
+++ src/.vitepress/nav/design-pattern.ts
@@ -4,6 +4,7 @@ const designPattern: DefaultTheme.SidebarItem[] = [
     { text: '设计模式', link: 'index.html' },
     { text: '面向对象设计', link: 'oop.html' },
     { text: 'UML 类图和时序图', link: 'uml-diagram.html' },
+    { text: '设计原则', link: 'design-principle.html' },
 ];
 
 export default designPattern;
