ec4978f1194f64a9d7031dee7d907c46239ea9ea
Kisstar <dwh.chn@foxmail.com>
2026-04-18 21:17:14 +0800

    docs(css): 添加高级CSS语法

--- src/.vitepress/nav/css.ts
+++ src/.vitepress/nav/css.ts
@@ -1,9 +1,10 @@
 import { type DefaultTheme } from 'vitepress';
 
 const CSSSidebar: DefaultTheme.SidebarItem[] = [
-    { text: 'CSS', link: 'index.html' },
-    { text: 'Flex 布局', link: 'flex.html' },
-    { text: 'Grid 布局', link: 'grid.html' },
+  { text: 'CSS', link: 'index.html' },
+  { text: 'Flex 布局', link: 'flex.html' },
+  { text: 'Grid 布局', link: 'grid.html' },
+  { text: '高级技巧', link: 'advanced-list.html' }
 ];
 
 export default CSSSidebar;
